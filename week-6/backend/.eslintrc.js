dfeac53c3327a226696daa37d591bc7e2fa8c4f2
johnalves <johnvictorio3@gmail.com>
2019-04-12 23:45:32 -0300

    v1.0

--- week-6/backend/.eslintrc.js
+++ week-6/backend/.eslintrc.js
@@ -2,15 +2,20 @@ module.exports = {
   env: {
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
-  extends: ['prettier', 'airbnb-base'],
+  extends: ["prettier", "airbnb-base"],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2018
   },
-  rules: { 'class-methods-use-this': 'off', 'no-param-reassign': 'off' },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "func-names": "off",
+    "no-underscore-dangle": "off"
+  }
 };
